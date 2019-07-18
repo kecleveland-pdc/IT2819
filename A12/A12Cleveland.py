fd12b55d953551d0896f5bc728204883cb460389
KEIGH\Keigh <ke.cleveland@gmail.com>
2019-07-18 19:14:28 -0400

    Added display_guesses function to store and print prior guesses.

--- A12/A12Cleveland.py
+++ A12/A12Cleveland.py
@@ -1,17 +1,35 @@
 # This is a guess the number game
 import random
+import array
+print("Program Written by Keaunna ", end="")
+print("Cleveland")
+
+def display_guesses(n: array) -> str:
+    input = "You already guessed "
+    
+    for guess in n:
+        if str(guess) in input:
+            continue
+        else:
+            input += str(guess) + " "
+    return input
 
 print ('Hello, what is your name?')
 name = input()
+guesses = []
 
 print ('Well, ' + name + ', I am thinking of a number between 1 and 20.')
 secretNumber = random.randint(1,20)
 print ('secretNumber = ' + str(secretNumber))
 
 for guessesTaken in range(1, 7):
-    print('Take a guess.')
+    print('Take a guess.\n')
     guess = int(input())
+    guesses.append(guess)
 
+    if guessesTaken > 1:
+        print(display_guesses(guesses))
+    
     if guess < secretNumber:
         print ('Your guess is too low.')
     elif guess > secretNumber:
@@ -20,6 +38,6 @@ for guessesTaken in range(1, 7):
         break # this indicates a correct guess
 
 if guess == secretNumber:
-    print('Good job, ' + name + '! Your guess the number in ' + str(guessesTaken) + ' guesses!')
+    print('Good job, ' + name + '! You guessed the number in ' + str(guessesTaken) + ' guesses!')
 else:
-    print('Nope. The numerber I was thinking of was ' + str(secretNumber))
+    print('Nope. The number I was thinking of was ' + str(secretNumber))
