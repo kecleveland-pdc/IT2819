845c901a922f9f8d44596321df8c5b04188d93ef
KEIGH\Keigh <ke.cleveland@gmail.com>
2019-07-25 20:16:33 -0400

    Adding initial file.

---
+++ A14/A14Cleveland.py
@@ -0,0 +1,29 @@
+# Programming Courses Game
+
+# Mainline
+
+def main():
+    programming_courses={"Computer Concepts":"IT 1025",\
+        "Programming Logic":"IT 1050",\
+        "Java Programming":"IT 2670",\
+        "C++ Programming":"IT 2650",\
+        "Python Programming":"IT 2800"}
+
+    print ("Learn your programming courses!\n")
+
+    correct=0
+    incorrect=0
+  
+    # Game Loop
+
+
+	    
+    # Display correct and incorrect answers
+    print ("You missed ",incorrect," courses.")
+    print ("You got ",correct," courses.\n")
+
+# Entry Point
+response=""
+while (response!="n"):
+    main()
+    response=input("\n\nPlay again?(y/n)\n# ")
