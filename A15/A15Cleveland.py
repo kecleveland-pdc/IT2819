cb53f904e3fe54be837c06b539798efc23a92bc6
KEIGH\Keigh <ke.cleveland@gmail.com>
2019-07-29 18:18:27 -0400

    Added check for insufficient fund + test values to check class

--- A15/A15Cleveland.py
+++ A15/A15Cleveland.py
@@ -8,15 +8,21 @@ class AccountP(object):
         self._balance += amount
 
     def withdraw(self, amount):
-        self._balance -= amount
+        if amount > self._balance:
+            print("Insufficient funds!!\n")
+        else:
+            self._balance -= amount
 
     def get_balance(self):
         return self._balance
 
     def dump(self):
-        print ("Dump Account Info")
-        s = '%s, %s, balance: %s\n' % (self._name, self._no, self._balance)
-        print (s)
-        
+        s = ("Dump Account Info: {0}, {1}, balance of {2}\n\nFinal balance: {3}".format(self._name, self._no, self._balance, self._balance))
+        return s
 # Main
-print ("Assignment 15 - written by Matt Weisfeld")
+print ("Assignment 15 - written by Keaunna Cleveland\n")
+JohnSmith = AccountP('John Smith', '19371554951', 20000)
+JohnSmith.deposit(1000)
+JohnSmith.withdraw(4000)
+JohnSmith.withdraw(3500)
+print(JohnSmith.dump())
